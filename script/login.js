d83d7b1adec682f06082de98e272054a47f8441f
Kiorn Pal <kironpal1531@gmail.com>
2026-04-03 21:25:52 +0600

    login function assign to home page

--- script/login.js
+++ script/login.js
@@ -1,1 +1,23 @@
 console.log("hello ")
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    //taking the mobile number value
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+
+     //taking the piin number value
+    const pinInput = document.getElementById("input-pin");
+    const pinNumber = pinInput.value;
+    console.log(pinNumber);
+
+    //matching the values
+    if(contactNumber ==="00000000000" && pinNumber === "1234"){
+        alert("login Sucessfull");
+        window.location.assign("/home.html");
+    }
+    else{
+        alert("Invalid credentials. Please try again.");
+        return;
+    }
+});
