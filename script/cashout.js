aeda6cc66392b5da4c5895e2137481b50ea4dab4
Kiorn Pal <kironpal1531@gmail.com>
2026-04-04 22:47:04 +0600

    cashout function add with balance calculation functionality and made the header sticky

--- script/cashout.js
+++ script/cashout.js
@@ -1,3 +1,44 @@
 document.getElementById("cashout-btn").addEventListener("click", function() {
+    const cashoutNumberInput = document.getElementById("cashout-number");
+    const cashoutNumber = cashoutNumberInput.value;
+    console.log(cashoutNumber);
+    if(cashoutNumber.length !== 11) {
+        alert("Please enter a valid phone number");
+        return;
+    }
+
+    const cashoutAmountInput = document.getElementById("cashout-amount");
+    const cashoutAmount = cashoutAmountInput.value;
+    console.log(cashoutAmount);
+
+
+
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    console.log(balance);
+
+    const newBlance = Number(balance) - Number(cashoutAmount);
     
-}
+
+    if(newBlance < 0) {
+        alert("Your balance is not enough");
+        return;
+    }
+
+
+
+
+
+    const cashoutPinInput = document.getElementById("cashout-pin");
+    const Pin = cashoutPinInput.value;
+    if(Pin === "1234") {
+        alert("Cashout Done");
+          console.log(newBlance);
+            balanceElement.innerText = newBlance;
+        
+    } else {
+        alert("wrong Pin");
+        return;
+    }
+
+});
